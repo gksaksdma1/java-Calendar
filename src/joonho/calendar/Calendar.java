3acb470c10e9f6ca21df897a2190e7a421ea5b0e
gksaksdma1@naver.com <gksaksdma1@naver.com>
2017-12-11 22:55:32 +0900

    마지막 일 구하기

--- src/joonho/calendar/Calendar.java
+++ src/joonho/calendar/Calendar.java
@@ -1,9 +1,36 @@
 package joonho.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		String PROMPT = "cal> ";
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		
+		int month = 1;
+		while ( true ) {
+		System.out.println("달을 입력하세요");
+		System.out.print(PROMPT);
+		month = scanner.nextInt();
+		if (month == -1) {
+			break;
+		}
+		
+		if (month > 12) {
+			continue; // 루프의 처음으로
+		}
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println("bye~");
+		scanner.close();
 	}
 
 }
---
+++ src/joonho/calendar/Sum.java
@@ -0,0 +1,25 @@
+package joonho.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+			
+		int a,b;
+		Scanner scanner = new Scanner(System.in);
+		String s1,s2;
+		System.out.println("두 수를 입력해 주세요.");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		System.out.println(s1 + ", "+ s2);
+		
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		
+		System.out.println("두 수의 합은 " + (a+b));
+		
+		scanner.close();
+	}
+
+}
